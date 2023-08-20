dbb1eb6e2c6aad74ba29d12bcf498020666718f2
toksJ <tokajaber11@gmail.com>
2023-08-20 23:56:40 +0400

    Initial Commit

---
+++ eleosQuestions/eleosQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  eleosQuestionsApp.swift
+//  eleosQuestions
+//
+//  Created by TokaJaber on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct eleosQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
